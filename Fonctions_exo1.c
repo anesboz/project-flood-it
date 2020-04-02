a46d770b2b7044c2f6bd960004c45df5652ecd2a
Nam Le <lehainam2371999@gmail.com>
2020-04-02 09:16:39 +0200

    first commit

---
+++ Fonctions_exo1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include "Entete_Fonctions.h"
+
+void trouve_zone_rec(int **M, int nbcase, int i, int j, int *taille, ListeCase *L){
+
+}
+
+
+int sequence_aleatoire_rec(int **M, Grille *G, int dim, int nbcl, int aff){
+  int cpt=0;
+
+  return cpt;
+}
+
+
